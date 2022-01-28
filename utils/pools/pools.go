d0b7b93dc22d28605f51ba4ea10e94c37514980c
J.G <gatheringstr@swaytheworld.com>
2022-01-28 01:26:54 -0600

    Correct function export

--- utils/pools/pools.go
+++ utils/pools/pools.go
@@ -12,7 +12,7 @@ import (
 // Generate a URL for a pool.
 // @param tx - The active database transaction/connection
 // @param algoID - The database ID for the algorithm to use for pool lookup
-func generatePoolURL(tx *gorm.DB, algoID uint64) string {
+func GeneratePoolURL(tx *gorm.DB, algoID uint64) string {
 	// Get a pool for the algorithm.
 	var pool Pool
 	tx.Where("algorithm_id = ?", algoID).Limit(1).Find(&pool)
