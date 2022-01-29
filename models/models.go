e2c78f7e8b832494451932102a9b363ce1dd4de7
J.G <gatheringstr@swaytheworld.com>
2022-01-28 22:03:10 -0600

    MODELS: Update version to force updates

--- models/models.go
+++ models/models.go
@@ -9,7 +9,7 @@ import (
 // ====================================
 
 // This is used to determine if the schema should be updated.
-const SchemaVersion = 1.04
+const SchemaVersion = 1.05
 
 // A mining algorithm such as scrypt.
 // Making this distinct will allow mapping between various pools and mining software.
